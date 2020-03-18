8cecb4e7fce62f1b949bb279a2a6ee291002f088
krksmsk <59610126+krksmsk@users.noreply.github.com>
2020-03-18 17:54:57 +0900

    complete_genre_index&create

--- app/controllers/admins/genres_controller.rb
+++ app/controllers/admins/genres_controller.rb
@@ -1,10 +1,18 @@
 class Admins::GenresController < ApplicationController
 	def index
-		
+		@genre = Genre.new
+		@genres = Genre.all
 	end
 
 	def create
-		
+	@genre = Genre.new(genre_params)
+		if @genre.save
+			redirect_to admins_root_path
+			flash[:notice] = "Genre was successfully created."
+		else
+			redirect_to admins_root_path
+			flash[:notice] = "Genre was not successfully created."
+		end
 	end
 
 	def edit
@@ -14,4 +22,10 @@ class Admins::GenresController < ApplicationController
 	def update
 		
 	end
+
+	private
+
+	def genre_params
+  		params.require(:genre).permit(:genre_name, :flag_active)
+  	end
 end
