e27b08119fd28785dfcbf5a616eac156fea7b54a
krksmsk <krks_0707@yahoo.co.jp>
2020-03-18 09:15:36 +0000

    complete_genres

--- app/controllers/admins/genres_controller.rb
+++ app/controllers/admins/genres_controller.rb
@@ -7,20 +7,27 @@ class Admins::GenresController < ApplicationController
 	def create
 	@genre = Genre.new(genre_params)
 		if @genre.save
-			redirect_to admins_root_path
+			redirect_to admins_genres_path
 			flash[:notice] = "Genre was successfully created."
 		else
-			redirect_to admins_root_path
+			redirect_to admins_genres_path
 			flash[:notice] = "Genre was not successfully created."
 		end
 	end
 
 	def edit
-		
+		@genre = Genre.find(params[:id])
 	end
 
 	def update
-		
+		@genre = Genre.find(params[:id])
+  		if @genre.update(genre_params)
+  			redirect_to admins_genres_path
+  			flash[:notice] = "Genre was successfully update."
+  		else
+  			render "edit"
+  			flash[:notice] = "Genre was not successfully apdate."
+  		end
 	end
 
 	private
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,2 +1,6 @@
 class Genre < ApplicationRecord
+	has_many :products, dependent: :destroy
+
+	validates :genre_name, presence: true
+	validates :flag_active, presence: true
 end
