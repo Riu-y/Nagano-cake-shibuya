3802214abb330b35fa8eb2e8713d00813859e9cb
taniguchi-yuta <w.08035781696.crip@gmail.com>
2020-03-21 05:26:49 +0000

    Stsrt_admin_customer

--- app/controllers/admins/customers_controller.rb
+++ app/controllers/admins/customers_controller.rb
@@ -1,3 +1,18 @@
 class Admins::CustomersController < ApplicationController
 	layout 'admins'
+	def index
+		@customers = Customer.all
+	end
+
+	def show
+		
+	end
+
+	def edit
+		
+	end
+
+	def update
+		
+	end
 end
