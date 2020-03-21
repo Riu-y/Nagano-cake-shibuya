c4cf76543efcd3dbcb70aad6d269912ab1ddca06
taniguchi-yuta <w.08035781696.crip@gmail.com>
2020-03-21 06:22:13 +0000

    Create_50%_admin_customer

--- app/controllers/admins/customers_controller.rb
+++ app/controllers/admins/customers_controller.rb
@@ -5,14 +5,20 @@ class Admins::CustomersController < ApplicationController
 	end
 
 	def show
-		
+		@customer = Customer.find(params[:id])
 	end
 
 	def edit
-		
+		@customer = Customer.find(params[:id])
 	end
 
 	def update
-		
+		@customer = Customer.find(params[:id])
+		@customer.update(customer_params)
+		redirect_to admins_customer_path(@customer.id)
+	end
+	private
+	def customer_params
+	    params.require(:customer).permit(:family_name, :first_name, :family_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :email, :is_acctive)
 	end
 end
