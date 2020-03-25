6487b493a65ec78f185841443a852f43e3709c29
taniguchi-yuta <w.08035781696.crip@gmail.com>
2020-03-25 07:57:28 +0000

    create is_acctive.false_reject

--- app/controllers/customers/sessions_controller.rb
+++ app/controllers/customers/sessions_controller.rb
@@ -3,6 +3,7 @@
 class Customers::SessionsController < Devise::SessionsController
   layout 'customers'
   # before_action :configure_sign_in_params, only: [:create]
+  before_action :reject_user, only: [:create]
 
   # GET /resource/sign_in
   # def new
@@ -19,7 +20,17 @@ class Customers::SessionsController < Devise::SessionsController
     #   new_customers_session_path
     # end
 
-  # protected
+  protected
+
+  def reject_user
+    @customer = Customer.find_by(email: params[:customer][:email].downcase)
+    if @customer
+      if (@customer.valid_password?(params[:customer][:password]) && (@customer.active_for_authentication? == false))
+        flash[:error] = "退会済みです。"
+        redirect_to new_customer_session_path
+      end
+    end
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
