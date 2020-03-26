5459e84e2b95a90b620b25b9175af30c3429ccc6
krksmsk <krks_0707@yahoo.co.jp>
2020-03-26 10:29:38 +0000

    complete_admin_order_item

--- app/controllers/admins/order_items_controller.rb
+++ app/controllers/admins/order_items_controller.rb
@@ -4,9 +4,23 @@ class Admins::OrderItemsController < ApplicationController
 		@order_details = OrderDetail.all
 	end
 	def show
-		
+		@order_detail = OrderDetail.find(params[:id])
+		@order_items = @order_detail.order_items
+
 	end
+
 	def update
-		
+		@order_item = OrderItem.find(params[:id])
+		#binding.pry
+		@order_item.update(order_item_params)
+		#binding.pry
+		redirect_back(fallback_location: admins_root_path)
+ 	end
+
+ 	private
+
+ 	def order_item_params
+		params.require(:order_item).permit(:creation_status)
 	end
+
 end
