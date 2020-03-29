af97db88f4931919308f43a0e305b40ab9a7c2fc
taniguchi-yuta <w.08035781696.crip@gmail.com>
2020-03-29 10:44:51 +0000

    add_pre_view

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,18 @@
 //= require jquery
 //= require bootstrap-sprockets
 //= require_tree .
+
+$( document ).on('turbolinks:load', function() {
+    function readURL(input) {
+        if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+    $('#img_prev').attr('src', e.target.result);
+        }
+        reader.readAsDataURL(input.files[0]);
+        }
+    }
+    $("#user_img").change(function(){
+        readURL(this);
+    });
+  });
